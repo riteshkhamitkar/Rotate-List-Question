9e997c6621efd8ebee31e71c82f4762396579277
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-08-02 07:17:01 +0530

    Add files via upload

---
+++ Rotate List Question (L-61).java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k ==0) return head;
+        int length =0;
+        ListNode tail = head;
+        while(tail.next != null){
+            length++;
+            tail = tail.next;
+        }
+        length++;
+        tail.next = head;
+        int rotatePoint = length - k%length;
+        tail = head;
+        while(rotatePoint-->1){
+            tail = tail.next;
+            
+        }
+        head = tail.next;
+        tail.next = null;
+        return head;
+    }
+}
